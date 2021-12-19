93fef746a7a87975af6c78fc3dc97f150def6a5d
giljunqueira-max <83184039+giljunqueira-max@users.noreply.github.com>
2021-12-19 08:30:29 -0600

    added timeseries

--- src/ProgrammingProjects.jl
+++ src/ProgrammingProjects.jl
@@ -2,16 +2,27 @@ module ProgrammingProjects
 
 using HTTP
 using Gumbo 
+using Cascadia
 
 include("webcrawler.jl")
-using .webcrawler: PAGE_URL
-using .webcrawler: LINKS
+# using .webcrawler: PAGE_URL
+# using .webcrawler: LINKS
 using .webcrawler: fetchpage
+using .webcrawler: extractlinks
+using .webcrawler: extracttitle
+using .webcrawler: extractimage
+using .webcrawler: articleinfo
+using .webcrawler: articledom
 
-
-export PAGE_URL
-export LINKS
+# export PAGE_URL
+# export LINKS
 export fetchpage
+export extractlinks
+export extracttitle
+export extractimage
+export articleinfo
+export articledom
+
 
 
 
--- src/webcrawler.jl
+++ src/webcrawler.jl
@@ -3,27 +3,91 @@ module webcrawler
 
 using HTTP
 using Gumbo
+using Cascadia
+using .Cascadia: nodeText
+import Cascadia: matchFirst 
+import Cascadia: nodeText
 
 
+export fetchrandom, fetchpage, articleinfo 
 
 const PAGE_URL = "https://en.wikipedia.org/wiki/Julia_(programming_language)" 
-
 const LINKS = String[] 
+ 
+function fetchpage(url) 
+  url = startswith(url, "/") ? buildurl(url) : url 
+ 
+  response = HTTP.get(url) 
+ 
+  if response.status == 200 && length(response.body) > 0 
+    String(response.body) 
+  else  
+    "" 
+  end 
+end 
+ 
 
-function fetchpage(url)
-    response = HTTP.get(url)
-    if response.status == 200 && parse(Int, Dict(response.headers)["Content-Length"]) > 0
-        String(response.body)
-    else
-        ""
-    end
+
+function extractlinks(elem) 
+  if  isa(elem, HTMLElement) &&  tag(elem) == :a && in("href", collect(keys(attrs(elem)))) 
+      url = getattr(elem, "href") 
+      startswith(url, "/wiki/") && push!(LINKS, url) 
+  end 
+ 
+  for child in children(elem) 
+    extractlinks(child) 
+  end 
+end 
+ 
+
+
+function extracttitle(elem) 
+  matchFirst(Selector("#section_0"), elem) |> nodeText 
+end 
+ 
+
+
+function extractimage(elem) 
+  e = matchFirst(Selector(".content a.image img"), elem) 
+  isa(e, Nothing) ? "" : e.attributes["src"] 
+end 
+ 
+
+
+function fetchrandom() 
+  fetchpage(RANDOM_PAGE_URL) 
 end 
 
 
 
+function articledom(content) 
+  if ! isempty(content) 
+    return Gumbo.parsehtml(content) 
+  end 
+ 
+  error("Article content can not be parsed into DOM") 
+end 
+ 
+
+
+function articleinfo(content) 
+  dom = articledom(content) 
+ 
+  Dict( :content => content,  
+        :links => extractlinks(dom.root),  
+        :title => extracttitle(dom.root),  
+        :image => extractimage(dom.root) 
+  ) 
+end 
+ 
+
+
+function buildurl(article_url) 
+  PROTOCOL * DOMAIN_NAME * article_url 
+end 
 
 
 
 end
 
-HTTP
+
